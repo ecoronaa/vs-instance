3264679e8258fc880f4e50597fe5a4316eb27c89
ecoronaa <157056123+ecoronaa@users.noreply.github.com>
2024-05-24 21:51:12 +0000

    added testAreaId

--- Snippets.ps1
+++ Snippets.ps1
@@ -8,14 +8,24 @@ function GetUrl() {
     $orgResourceAreasUrl = [string]::Format("{0}/_apis/resourceAreas/{1}?api-version=6.0-preview.1", $orgUrl, $AreaId)
 
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    Invoke-RestMethod -Uri $orgResourceAreasUrl -Headers $header
+    $results = Invoke-RestMethod -Uri $orgResourceAreasUrl -Headers $header
+
+    if ("null" -eq $results) {
+        $areaUrl = $orgUrl
+    }
+    else {
+        $areaUrl = $results.locationUrl
+    }
+
+    return $areaUrl
 }
 
 $orgUrl = $ORG_URL
 $personalToken = $PAT
+$testAreaId = "3b95fb80-fdda-4218-b60e-1052d070ae6b"
 
 Write-Host "Initialize authentication context" -ForegroundColor Yellow
 $token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($personalToken)"))
 $header = @{authorization = "Basic $token" }
 
-GetUrl -orgUrl $orgUrl -header $header -AreaId $testAreaId
+$tfsBaseUrl = GetUrl -orgUrl $orgUrl -header $header -AreaId $testAreaId
