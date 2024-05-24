5ef1689a20f601428259fbf17d3e088e2b0e4e64
ecoronaa <157056123+ecoronaa@users.noreply.github.com>
2024-05-24 21:41:25 +0000

    added GetUrl() simple

--- Snippets.ps1
+++ Snippets.ps1
@@ -1,3 +1,16 @@
+function GetUrl() {
+    param(
+        [string]$orgUrl,
+        [hashtable]$header,
+        [string]$AreaId
+    )
+
+    $orgResourceAreasUrl = [string]::Format("{0}/_apis/resourceAreas/{1}?api-version=6.0-preview.1", $orgUrl, $AreaId)
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-RestMethod -Uri $orgResourceAreasUrl -Headers $header
+}
+
 $orgUrl = $ORG_URL
 $personalToken = $PAT
 
@@ -5,8 +18,4 @@ Write-Host "Initialize authentication context" -ForegroundColor Yellow
 $token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($personalToken)"))
 $header = @{authorization = "Basic $token" }
 
-$orgResourceAreasUrl = [string]::Format("{0}/_apis/resourceAreas/{1}?api-version=6.0-preview.1", $orgUrl, $AreaId)
-
-# Do a GET on this URL (this returns an object with a "locationUrl" field)
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-RestMethod -Uri $orgResourceAreasUrl -Headers $header
+GetUrl -orgUrl $orgUrl -header $header -AreaId $testAreaId
